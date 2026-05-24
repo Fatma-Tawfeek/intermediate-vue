e79fe866dc45b3742cfc29d30b26b2fbf97c5f95
Fatma-Tawfeek <fatmatawfeek97@gmail.com>
2026-05-24 04:21:48 +0300

    feat: add sandbox page and base text input component

---
+++ app/src/components/BaseTextInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-control">
+    <label class="label" :for="id">
+      <span class="label-text font-medium">{{ label }}</span>
+    </label>
+    <input
+      :id="id"
+      class="input input-bordered w-full"
+      type="text"
+      placeholder="Enter task title..."
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped></style>
---
+++ app/src/views/SandBoxPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>{{ name }}</p>
+  <BaseTextInput id="name" label="Name" v-model="name" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseTextInput from '@/components/BaseTextInput.vue'
+
+const name = ref('')
+</script>
+
+<style scoped></style>
